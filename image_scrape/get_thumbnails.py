a5e870ac3dcd5e0e0f8ef18890aed5d138cfac8c
Gary Foreman <gjman@stigsfruitycousin.local>
2016-08-21 17:28:07 -0500

    Add code to actually scrape images. Need to add sleep functionality!

--- image_scrape/get_thumbnails.py
+++ image_scrape/get_thumbnails.py
@@ -8,14 +8,24 @@ Guitars forum at talkbass.com
 """
 
 from __future__ import print_function
-import csv
+import csv, os, urllib
 import numpy as np
 from pyquery import PyQuery as pq
+
+DATA_PATH = os.path.join('..', 'data')
 
 INDEX_TALKBASS = 'http://www.talkbass.com/'
 IMAGES_TALKBASS = 'http://images'
 CLASSIFIEDS = "forums/for-sale-bass-guitars.126/"
 NUM_PAGES = 1
+
+def make_data_dir():
+    """
+    Checks to see whether DATA_PATH exists. If not, creates it.
+    """
+
+    if not os.path.isdir(DATA_PATH):
+        os.makedirs(DATA_PATH)
 
 def get_page_url(i):
     """
@@ -40,16 +50,36 @@ def get_thumb_urls(d):
 
     return thumbnail_url_list
 
-def main():
-    thumbnail_url_list = []
+def download_thumb(thumbnail_url):
+    """
+    thumbnail_url : a string with a url to a bass image
+    Pulls dowm image from thumbnail_url and stores in DATA_DIR
+    """
 
+    print(thumbnail_url)
+
+    filename = thumbnail_url.split('/')[-1]
+
+    try:
+        urllib.urlretrieve(thumbnail_url, os.path.join(DATA_PATH, filename))
+    except IOError:
+        #URL is not an image file
+        pass
+
+def main():
+    make_data_dir()
+    
     for i in xrange(1, NUM_PAGES+1):
+        thumbnail_url_list = []
+
         tb_classified_page = get_page_url(i)
 
         #initialize PyQuery
         d = pq(tb_classified_page)
 
         thumbnail_url_list.extend(get_thumb_urls(d))
+
+        map(download_thumb, thumbnail_url_list)
 
         print("page", i, "finished")
 
